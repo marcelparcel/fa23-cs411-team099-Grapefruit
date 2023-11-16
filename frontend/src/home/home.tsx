f9cfbf182fcdaf8d4d1ea9c524409d0dc778e20f
Adriana Shuaipaj <ashua2@illinois.edu>
2023-11-16 13:50:03 -0600

    added login and home folders - frontend

---
+++ frontend/src/home/home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './home.css';
+
+export default function HomeView() {
+    return (
+        <div>
+            {/* this is a placeholder */}
+            <h1>Home</h1> 
+        </div>
+    );
+}
---
+++ frontend/src/login/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './login.css';
+
+export default function LoginView() {
+    return (
+        <div>
+            {/* this is a placeholder */}
+            <h1>Login</h1> 
+        </div>
+    );
+}
+
