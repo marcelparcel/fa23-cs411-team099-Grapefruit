e68c4fe8b406b56385a65ff0d8a22567a61d8af3
jrang3 <89742677+jrang3@users.noreply.github.com>
2023-12-01 19:22:15 -0600

    Fixed Stored Procedure Issue

--- backend/sp1.sql
+++ backend/sp1.sql
@@ -3,17 +3,17 @@ CREATE PROCEDURE Result()
 BEGIN
  	Declare varHeadsign VARCHAR(255);
 	Declare varCountTripId INT;
-	Declare varAvgRoutPrice Real;
+	Declare varAvgRoutePrice REAL;
 	Declare varOptionsClassification VARCHAR(255);
 	
 	Declare exit_loop BOOLEAN default FALSE;
-	
+
 	Declare tripCur CURSOR FOR(SELECT Trip.Headsign, COUNT(DISTINCT Trip.TripId), AVG(Route.Price) 
 	AS avgRoutePrice
-	FROM `411-project` . `Trip`
-	JOIN `411-project` . `StopsToTrips` ON StopsToTrips.TripId = Trip.TripId
-	JOIN `411-project` . `Stop` ON StopsToTrips.StopId = Stop.StopId
-	JOIN `411-project` . `Route` ON Trip.RouteId = Route.RouteId 
+	FROM  Trip
+	JOIN StopsToTrips ON StopsToTrips.TripId = Trip.TripId
+	JOIN Stop ON StopsToTrips.StopId = Stop.StopId
+	JOIN Route ON Trip.RouteId = Route.RouteId 
 	GROUP BY Trip.Headsign
 	ORDER BY avgRoutePrice ASC);
 
@@ -29,18 +29,18 @@ BEGIN
   );
 	Open tripCur;
 		cloop: LOOP
-		FETCH tripCur Into varHeadsign, varCountTripId, varAvgRoutePrice Real
+		FETCH tripCur Into varHeadsign, varCountTripId, varAvgRoutePrice;
 	IF exit_loop THEN
         Leave cloop;
     END IF;
 	IF varCountTripId >= 30 THEN
-		SET varOptionsClassification = “Several Options”
+		SET varOptionsClassification = "Several Options";
 	ELSEIF varCountTripId >= 20 THEN
-		SET varOptionsClassification = “Decent Amount Options”
+		SET varOptionsClassification = "Decent Amount Options";
 	ELSEIF varCountTripId >= 10 THEN
-		SET varOptionsClassification = “ Limited Options”
+		SET varOptionsClassification = "Limited Options";
 	ELSE
-		SET varOptionsClassification = “ Very Limited Options”
+		SET varOptionsClassification = "Very Limited Options";
 
 	END IF;
 	Insert INTO NewTable Value(varHeadsign, varAvgRoutePrice,  varCountTripId,  varOptionsClassification);
@@ -48,6 +48,7 @@ BEGIN
 	Close tripCur;
 
 	Select Headsign, CountTripId, AvgRoutePrice, OptionsClassification
-	From NewTable
-END;
-DELIMITER;
+	From NewTable;
+
+END $$
+DELIMITER ;
