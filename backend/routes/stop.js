06ee69f63d58132034e50a346dece7ae23b55036
Marcel Hoang <marcelh2@illinois.edu>
2023-12-02 19:06:38 -0600

    updated stop

--- backend/routes/stop.js
+++ backend/routes/stop.js
@@ -20,4 +20,41 @@ stop.get('/', async function(req, res) {
     }
 });
 
+/* Add two stops to StopsPicked */
+stop.post('/', async function(req, res) {
+    if (!req.body.stop1 || !req.body.stop2) {
+        return res.status(400).json({ message: 'Missing Stop 1 or Stop 2' });
+    }
+    var conn = await getConn;
+    var query = 'INSERT INTO StopsPicked(StopId1, StopId2) VALUES (?, ?)';
+    try {
+        const rows = await conn.query(query, [req.body.stop1, req.body.stop2]);
+        conn.release();
+        return res.json(rows[0]);
+      } catch (err) {
+        console.error(err);
+        conn.release();
+        return res.status(500).json({ error: 'An error occurred while executing the query.' });
+    }
+});
+
+/* Delete from StopsPicked */
+stop.delete('/', async function(req, res) {
+    if (!req.body.stop1 || !req.body.stop2) {
+        return res.status(400).json({ message: 'Missing Stop 1 or Stop 2' });
+    }
+    var conn = await getConn;
+    var query = 'DELETE FROM StopsPicked WHERE StopId1 = ? AND StopId2 = ?';
+    try {
+        const rows = await conn.query(query, [req.body.stop1, req.body.stop2]);
+        conn.release();
+        return res.json(rows[0]);
+      } catch (err) {
+        console.error(err);
+        conn.release();
+        return res.status(500).json({ error: 'An error occurred while executing the query.' });
+    }
+});
+
+
 module.exports = stop;
