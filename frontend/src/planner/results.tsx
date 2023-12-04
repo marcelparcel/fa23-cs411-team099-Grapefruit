61479500e8a14462d23d7744dcf947501168d7fc
Marcel Hoang <marcelh2@illinois.edu>
2023-12-03 21:38:20 -0600

    start results

---
+++ frontend/src/planner/results.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import './results.css';
+
+const ResultsView = () => {
+    
+    
+    return (
+        <div>
+            <h1>Results</h1>
+        </div>
+    )
+}
+
+export default ResultsView;
