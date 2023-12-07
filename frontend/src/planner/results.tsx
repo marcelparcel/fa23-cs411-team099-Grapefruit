d63dfb95b6afa34ceb83d4f778019f6b2dca0c79
Adriana Shuaipaj <ashua2@illinois.edu>
2023-12-06 18:06:02 -0600

    used stored procedure

--- frontend/src/planner/results.tsx
+++ frontend/src/planner/results.tsx
@@ -18,10 +18,16 @@ let DefaultIcon = L.icon({
 L.Marker.prototype.options.icon = DefaultIcon;
 
 const ResultsView = () => {
+    const [classification, setClassification] = useState('');
+    const [averagePrice, setAveragePrice] = useState('');
+    const [headsign, setHeadsign] = useState('');
+
     const query = new URLSearchParams(useLocation().search);
     const stop1: number | null = Number(query.get('stop1'));
     const stop2: number | null = Number(query.get('stop2'));
     const trip = query.get('trip');
+    console.log("the tripid:")
+    console.log(trip);
 
     const getStopPoints = useCallback(async () => {
         var geojson: GeoJSON.FeatureCollection<GeoJSON.Point> = {
@@ -86,20 +92,65 @@ const ResultsView = () => {
 
     const [stopPoints, setStopPoints] = useState<GeoJSON.FeatureCollection<GeoJSON.Point> | null>(null);
     const [tripPath, setTripPath] = useState<GeoJSON.Feature<GeoJSON.LineString> | null>(null);
+    const findInProcedure = useCallback(async () => {
+        console.log("in findInProcedure");
+        try {
+            await axios.get('http://localhost:4000/sp?get=1').then(response => {
+                if (response.data) {
+                    console.log("in response.data..");
+                    response.data.forEach(result => {
+                        console.log("each result:")
+                        console.log(result);
+                        var tripIdsString = result.TripIds;
+
+                        const tripIds: string[] = tripIdsString.split(',');
+
+                        const isMatch: boolean = tripIds.includes(trip);
+
+                        if (isMatch) {
+                            console.log(`${trip} matches a tripId in the string.`);
+                            const classification = result.OptionsClassification;
+                            const average_price = result.AvgRoutePrice;
+                            console.log(classification);
+                            console.log(average_price);
+                            setAveragePrice(average_price);
+                            setClassification(classification);
+                            setHeadsign(result.Headsign)
+                            
+                        } else {
+                            console.log(`${trip} does not match any tripId in the string.`);
+                        }
+
+                    });
+                }
+                }).catch(function (error) {
+                if (error.response) {
+                  console.log(error.response);
+                }
+                
+              });
+        } catch (error) {
+            console.error('Error fetching stored procedure:', error);
+        }
+        
+    }, [trip]);
 
     useEffect(() => {
+        console.log("in useeffect");
         const fetchData = async () => {
             const stopPointsData = await getStopPoints();
             const tripPathData = await getTripPath();
             setStopPoints(stopPointsData);
             setTripPath(tripPathData);
+            await findInProcedure();
         };
         fetchData();
-    }, [getStopPoints, getTripPath]);
+    }, [getStopPoints, getTripPath, findInProcedure]);
 
     if (!stopPoints || !tripPath) {
         return <div>Loading...</div>;
     }
+
     
     var center: LatLngExpression = [stopPoints.features[0].geometry.coordinates[1], stopPoints.features[0].geometry.coordinates[0]];
 
@@ -129,7 +180,12 @@ const ResultsView = () => {
                     layer.bindPopup(feature.properties.popupContent);
                 }}/>
             </MapContainer>
+            <div className="maptext">
+                <h4 className="maptext">Take the {trip} towards {headsign}</h4>
+                <h5>Average Price: ${averagePrice}, Classification: {classification}</h5>
+            </div>
         </div>
+        
     )
 }
 
